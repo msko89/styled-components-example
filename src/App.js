7e4553f681e1ad875dbd6df10af28e2651bde7b0
msko <kms89123@gmail.com>
2021-06-06 20:18:50 +0900

    Styled-components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,64 @@
 import logo from './logo.svg';
 import './App.css';
+import StyledButton from './components/StyledButton';
+import styled, { createGlobalStyle } from 'styled-components';
+import StyledA from './components/StyledA';
+
+const PrimaryStyledButton = styled(StyledButton)`
+  background: palevioletred;
+  color: white;
+`;
+
+const UppercaseButton = (props) => (
+  <button {...props} children={props.children.toUpperCase()} />
+);
+
+const MyButton = (props) => (
+  <button {...props} children={`MyButton ${props.children}`} />
+);
+
+const StyledMyButton = styled(MyButton)`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid ${(props) => props.color || 'palevioletred'};
+  color: ${(props) => props.color || 'palevioletred'};
+  margin: 0 1em;
+  padding: 0.25em 1em;
+  font-size: 20px;
+
+  :hover {
+    border: 2px solid red;
+  }
+
+  ::before {
+    content: '@';
+  }
+`;
+
+const GlobalStyle = createGlobalStyle`
+  button {
+    color: yellow;
+  }
+`;
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <StyledButton>Button</StyledButton>
+          <StyledButton primary>Button</StyledButton>
+          <PrimaryStyledButton>Button</PrimaryStyledButton>
+          <StyledButton as="a" href="/">
+            Button
+          </StyledButton>
+          <StyledButton as={UppercaseButton}>Button</StyledButton>
+          <StyledMyButton>button</StyledMyButton>
+          <StyledMyButton color="green">button</StyledMyButton>
+          <StyledA href="https://google.com">태그</StyledA>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
